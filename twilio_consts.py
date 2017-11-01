8e78055ba6c661e78cdb3e222b41590456a6fe64
Atif Mahadik <atif@phantom.us>
2017-11-01 15:43:54 -0700

    PAPP-3402 1st Cut of the app

---
+++ twilio_consts.py
@@ -0,0 +1,17 @@
+# --
+# File: twilio_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2017
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+TWILIO_FINAL_STATUS = ["failed", "delivered", "undelivered"]
+TWILIO_TIMEOUT_MINUTES = 1
+TWILIO_SLEEP_SECS = 5
